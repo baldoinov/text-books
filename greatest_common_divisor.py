93347869ad9945f250cd5b53780ebe9a518b7564
Vitor Baldoino <baldoinov@users.noreply.github.com>
2021-12-28 20:40:33 -0300

    .

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -2,6 +2,30 @@
 divides both ofthem with no remainder.
 One way to find the GCD oftwo numbers is based on the observation that 
 if r is the remainder when a is divided by b, then gcd(a, b) = gcd(b, r). 
-As a base case, we can use gcd(a, 0) = a.
-Write a function called gcd that takes parameters a and b and returns their 
-greatest common divisor."""
+As a base case, we can use gcd(a, 0) = a. Write a function called gcd 
+that takes parameters a and b and returns their greatest common divisor."""
+
+def gcd(a, b):
+    
+    if b == 0:
+        return a
+    else:
+        r = a % b
+        return gcd(b, r)
+
+
+print("gcd(6, 3): ", gcd(6, 3))
+print("gcd(8, 4): ", gcd(8, 4))
+print("gcd(25, 22): ", gcd(25, 22))
+print("gcd(28, 30): ", gcd(28, 30))
+print("gcd(1, 2): ", gcd(1, 2))
+print("gcd(2, 1): ", gcd(2, 1))
+print("gcd(2, 2): ", gcd(2, 2)) 
+print("gcd(3, 2): ", gcd(3, 2))
+print("gcd(4, 2): ", gcd(4, 2)) 
+print("gcd(5, 2): ", gcd(5, 2))
+print("gcd(6, 2): ", gcd(6, 2))
+print("gcd(7, 2): ", gcd(7, 2))
+print("gcd(8, 2): ", gcd(8, 2)) 
+print("gcd(9, 2): ", gcd(9, 2))
+print("gcd(10, 2): ",gcd(10, 2))
--- is_power.py
+++ is_power.py
@@ -1,28 +1,37 @@
 """A number, a, is a power of b if it is divisible by b and a/b is a power of b. 
 Write a function called is_power that takes parameters a and b and returns True 
-if a is a power of b. Note: you will have to think about the base case."""
+if a is a power of b. Note: you will have to think about the base case.
+
+https://stackoverflow.com/questions/4738908/finding-out-whether-a-is-a-power-of-b
+"""
+
 
 def is_power(a, b):
+    """ This functions check if the number a is a power of b.
+    That is, if the following is true for a certain number n: a == b**n.
+    """
 
-    if (a <= 0) or (b <= 0):
-        return "A função não está definida para número menores ou iguais a 0."
-    
-    elif (a == 1) or (a == (a/b)):
+    if (a == 1):
         return True
-    
-    else: 
-        if ((a % b) == 0) and (is_power((a/b), b)):
+    elif (a == b):
+        return True
+    elif (a == 0) and (b != 0):
+        return False
+    elif (a != 1) and (b == 1):
+        return False
+    else:
+        if (a % b == 0) and is_power((a/b), b):
             return True
         else:
             return False
-    
+        
 
 print("is_power(0, 0): ", is_power(0, 0))
 print("is_power(1, 0): ", is_power(1, 0))
 print("is_power(0, 1): ", is_power(0, 1))
 print("is_power(1, 1): ", is_power(1, 1)) 
 print("is_power(1, 2): ", is_power(1, 2)) 
-print("is_power(2, 1): ", is_power(2, 1))   # TODO: Esse resultado está errado devido à condição (a == (a/b))
+print("is_power(2, 1): ", is_power(2, 1))
 print("is_power(2, 2): ", is_power(2, 2)) 
 print("is_power(3, 2): ", is_power(3, 2))
 print("is_power(4, 2): ", is_power(4, 2)) 
@@ -32,3 +41,5 @@ print("is_power(7, 2): ", is_power(7, 2))
 print("is_power(8, 2): ", is_power(8, 2)) 
 print("is_power(9, 2): ", is_power(9, 2))
 print("is_power(10, 2): ", is_power(10, 2))
+print("is_power(-10, 2): ", is_power(-10, 2))
+print("is_power(-8, -2): ", is_power(-8, -2))
